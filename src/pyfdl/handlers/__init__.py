779a0310c76b87ac4daa002c33794086d344f4da
apetrynet <flehnerheener@gmail.com>
2024-08-18 01:16:09 +0200

    * Adjusted to removal of directions * Exceptions now get raised in the registry
    Signed-off-by: apetrynet <flehnerheener@gmail.com>

--- src/pyfdl/handlers/__init__.py
+++ src/pyfdl/handlers/__init__.py
@@ -1,44 +1,31 @@
 from pathlib import Path
 from typing import Optional, Any, Union
 
-from pyfdl.errors import UnsupportedHandlerError
 from pyfdl import plugins
 
 
-def get_handler(direction: str, func_name: str, path: Path = None, handler_name: str = None) -> Any:
+def get_handler(func_name: str, path: Path = None, handler_name: str = None) -> Any:
     """
     Convenience function to get a matching handler matching the provided arguments.
 
     Args:
-        direction: of handler, "input" or "output"
         func_name: desired function name in handler
         path: to file. Used to get handler based on suffix
         handler_name: ask for a specific handler by name
 
     Returns:
-
+        handler:
     """
 
-    _manifest = plugins.get_registry()
-    handler = None
+    if handler_name is None and path is None:
+        raise RuntimeError(f'"handler_name" and "path" can\'t both be None. Please provide one or the other')
+
+    _registry = plugins.get_registry()
     if handler_name is not None:
-        handler = _manifest.get_handler_by_name(handler_name, direction=direction, func_name=func_name)
-        if handler is None:
-            raise AttributeError(
-                f'No handler by name: "{handler_name}" registered. '
-                f'Please consider using one of the following: {sorted(_manifest.handlers["inputs"])}'
-            )
+        handler = _registry.get_handler_by_name(handler_name, func_name=func_name)
 
-    elif path is not None:
-        handler = _manifest.get_handler_by_suffix(path.suffix, direction=direction, func_name=func_name)
-        if handler is None:
-            raise AttributeError(f'No handler supporting suffix: "{path.suffix}" registered.')
-
-    if handler is None:
-        raise UnsupportedHandlerError(
-            f'Unable to find an {direction} handler by name: "{handler_name}" or '
-            f'suitable handler for suffix: "{path.suffix}"'
-        )
+    else:
+        handler = _registry.get_handler_by_suffix(path.suffix, func_name=func_name)
 
     return handler
 
@@ -56,12 +43,7 @@ def read_from_file(path: Union[Path, str], handler_name: str = None, **handler_k
     Returns:
         FDL:
     """
-    handler = get_handler(
-        direction="input",
-        func_name='read_from_file',
-        path=path,
-        handler_name=handler_name
-    )
+    handler = get_handler(func_name='read_from_file', path=path, handler_name=handler_name)
 
     return handler.read_from_file(Path(path), **handler_kwargs)
 
@@ -79,11 +61,7 @@ def read_from_string(s: str, handler_name: str = 'fdl', **handler_kwargs: Option
     Returns:
         FDL:
     """
-    handler = get_handler(
-        direction="input",
-        func_name='read_from_string',
-        handler_name=handler_name
-    )
+    handler = get_handler(func_name='read_from_string', handler_name=handler_name)
     return handler.read_from_string(s, **handler_kwargs)
 
 
@@ -98,12 +76,7 @@ def write_to_file(fdl: 'FDL', path: Union[Path, str], handler_name: str = 'fdl',
         handler_name: name of handler to use
         **handler_kwargs: arguments passed to handler
     """
-    handler = get_handler(
-        direction="output",
-        func_name='write_to_file',
-        path=path,
-        handler_name=handler_name
-    )
+    handler = get_handler(func_name='write_to_file', path=path, handler_name=handler_name)
     handler.write_to_file(fdl, Path(path), **handler_kwargs)
 
 
@@ -120,9 +93,5 @@ def write_to_string(fdl: 'FDL', handler_name: str = 'fdl', **handler_kwargs: Opt
     Returns:
 
     """
-    handler = get_handler(
-        direction="output",
-        func_name='write_to_string',
-        handler_name=handler_name
-    )
+    handler = get_handler(func_name='write_to_string', handler_name=handler_name)
     return handler.write_to_string(fdl, **handler_kwargs)
