f02efdd8d3f4dec1417621103839e45f26f53ebc
apetrynet <flehnerheener@gmail.com>
2024-08-19 00:21:07 +0200

    * Removes old load and dump methods
    Signed-off-by: apetrynet <flehnerheener@gmail.com>

--- src/pyfdl/handlers/fdl_handler.py
+++ src/pyfdl/handlers/fdl_handler.py
@@ -87,69 +87,6 @@ class FDLHandler:
 
         return json.dumps(fdl.to_dict(), indent=indent, sort_keys=False)
 
-    def load(self, fp: IO, validate: bool = True) -> FDL:
-        """
-        Load an FDL from a file.
-
-        Args:
-            fp: file pointer
-            validate: validate incoming json with jsonschema
-
-        Raises:
-            jsonschema.exceptions.ValidationError: if the contents doesn't follow the spec
-
-        Returns:
-            FDL:
-        """
-
-        raw = fp.read()
-        return self.loads(raw, validate=validate)
-
-    def loads(self, s: str, validate: bool = True) -> FDL:
-        """Load an FDL from string.
-
-        Args:
-            s: string representation of an FDL
-            validate: validate incoming json with jsonschema
-
-        Returns:
-            FDL:
-
-        """
-        fdl = FDL.from_dict(json.loads(s))
-
-        if validate:
-            fdl.validate()
-
-        return fdl
-
-    def dump(self, obj: FDL, fp: IO, validate: bool = True, indent: Union[int, None] = 2):
-        """Dump an FDL to a file.
-
-        Args:
-            obj: object to serialize
-            fp: file pointer
-            validate: validate outgoing json with jsonschema
-            indent: amount of spaces
-        """
-        fp.write(self.dumps(obj, validate=validate, indent=indent))
-
-    def dumps(self, obj: FDL, validate: bool = True, indent: Union[int, None] = 2) -> str:
-        """Dump an FDL to string
-
-        Args:
-            obj: object to serialize
-            validate: validate outgoing json with jsonschema
-            indent: amount of spaces
-
-        Returns:
-            string: representation of the resulting json
-        """
-        if validate:
-            obj.validate()
-
-        return json.dumps(obj.to_dict(), indent=indent, sort_keys=False)
-
 
 def register_plugin(registry: 'PluginReistry'):
     """
